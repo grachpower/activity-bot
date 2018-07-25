0bbd9675a292f19b8f18c1efef878fe041b16cb9
DESKTOP-DA70CJK\Artem.Andreev <Artem.Andreev@clouty.ru>
2018-07-25 14:30:04 +0300

    add bot

--- main.go
+++ main.go
@@ -1,9 +1,50 @@
 package main
 
 import (
-	"fmt"
+	"github.com/Syfaro/telegram-bot-api"
+	"log"
 )
 
+const BOT_TOKEN = "646189272:AAEFTGLNqqVXZc_RKCtwC5gJ7XlQBJR7XLA"
+
 func main () {
-	fmt.Println("Hello world")
+	bot, err := tgbotapi.NewBotAPI(BOT_TOKEN)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	// инициализируем канал, куда будут прилетать обновления от API
+	ucfg := tgbotapi.NewUpdate(0)
+	ucfg.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(ucfg)
+
+	for {
+		select {
+		case update := <-updates:
+			// Пользователь, который написал боту
+			UserName := update.Message.From.UserName
+
+			// ID чата/диалога.
+			// Может быть идентификатором как чата с пользователем
+			// (тогда он равен UserID) так и публичного чата/канала
+			ChatID := update.Message.Chat.ID
+
+			// Текст сообщения
+			Text := update.Message.Text
+
+			log.Printf("[%s] %d %s", UserName, ChatID, Text)
+
+			// Ответим пользователю его же сообщением
+			reply := Text
+			// Созадаем сообщение
+			msg := tgbotapi.NewMessage(ChatID, reply)
+			// и отправляем его
+			bot.Send(msg)
+		}
+
+	}
 }
